8c0b4444998e53555e71b813cc288d4f101cc724
zedaenious <matt@zelenak.me>
2025-03-14 16:32:53 -0400

    replace assets; stub tabs nav

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Tabs } from 'expo-router'
+
+const _Layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          title: 'Home'
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          headerShown: false,
+          title: 'Search'
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          headerShown: false,
+          title: 'Saved'
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: false,
+          title: 'Profile'
+        }}
+      />
+    </Tabs>
+  )
+}
+
+export default _Layout
